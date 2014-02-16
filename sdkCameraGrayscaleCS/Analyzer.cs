ede34288196b354faa1ff6989692afd9c3d964d4
kfirt <kfir.tishbi@gmail.com>
2014-02-16 13:40:12 +0200

    Forgot analyzed

---
+++ sdkCameraGrayscaleCS/Analyzer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdkCameraGrayscaleCS
+{
+    public class Analyzer
+    {
+        public Analyzer()
+        {
+
+        }
+        public AnalyzedState process(int[] bitmap)
+        {
+            Array values = Enum.GetValues(typeof(AnalyzedState));
+            Random random = new Random();
+            return (AnalyzedState)values.GetValue(random.Next(values.Length));
+        }
+    }
+}
