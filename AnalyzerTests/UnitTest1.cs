c3a3c1a658bdfbdd74d316642b10a955967e113a
shairubin <shai_rubin@yahoo.com>
2014-02-17 11:05:34 +0200

    unit tests now compile

--- AnalyzerTests/UnitTest1.cs
+++ AnalyzerTests/UnitTest1.cs
@@ -4,6 +4,7 @@ using sdkCameraGrayscaleCS;
 using System.Drawing;
 using System.IO;
 using AnalyzeTrafficLight;
+using System.Collections.Generic; 
 
 namespace AnalyzerTests
 {
@@ -33,23 +34,26 @@ namespace AnalyzerTests
 
         private AnalyzedState runAnalyzer(string imagePath)
         {
-            //Uri uri = new Uri(imagePath, UriKind.RelativeOrAbsolute);
-            //Analyzer analyzer = new Analyzer();
+            Uri uri = new Uri(imagePath, UriKind.RelativeOrAbsolute);
+            Analyzer analyzer = new Analyzer();
             
-            //Bitmap image = new Bitmap(imagePath);
+            System.Drawing.Bitmap image = new System.Drawing.Bitmap(imagePath);
 
             //// Loop through the image
-            //int[] map = new int[(int)image.Width * (int)image.Height];
+            int[] map = new int[(int)image.Width * (int)image.Height];
             
-            //for (int x = 0; x < image.Width; x++)
-            //{
-            //    for (int y = 0; y < image.Height; y++)
-            //    {
-            //        Color pixelColor = image.GetPixel(x, y);
-            //        map[x*image.Width + y] = pixelColor.ToArgb();
-            //    }
-            //}
-            ////return analyzer.analyzeImage(map);
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    System.Drawing.Color pixelColor = image.GetPixel(x, y);
+                    map[x*image.Width + y] = pixelColor.ToArgb();
+                }
+            }
+           
+            List<AnalyzedObject> objectList = analyzer.analyzeImage(map, image.Width, image.Height); 
+            
+            
             return AnalyzedState.Red;
         }
     }
