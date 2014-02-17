ab5f3893f43c7eb373b4d8b13263fc4849fc7349
igorlyakas <igorlyakas@yahoo.com>
2014-02-17 11:58:21 +0200

    bounding box

--- AnalyzeTrafficLight/AnalyzedObject.cs
+++ AnalyzeTrafficLight/AnalyzedObject.cs
@@ -6,14 +6,29 @@ using System.Threading.Tasks;
 
 namespace AnalyzeTrafficLight
 {
-    public class AnalyzedObject
+    public class ObjPoint
     {
         public int x { get; set; }
         public int y { get; set; }
+    }
 
+    public class BoundingBox
+    {
+        public ObjPoint leftTop = new ObjPoint();
+        public ObjPoint rightBottom = new ObjPoint();
+    }
+
+    public class AnalyzedObject
+    {
+        public ObjPoint leftTop = new ObjPoint();
+        
         public int size { get; set; }
 
         public Color color { get; set; }
         public bool decision { get; set; }
+
+        public BoundingBox bBox = new BoundingBox();
+
+
     }
 }
